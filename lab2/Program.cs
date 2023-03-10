bfe87c308c804cdf076ba42734d06dab515a8f01
lursz <lukoszr@gmail.com>
2023-03-10 15:37:39 +0100

    eventual lab2 commit

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Runtime.InteropServices;
+using System;
 using System.Collections.Generic;
 using System.IO;
 
@@ -55,8 +56,6 @@ public class OsobaFizyczna : PosiadaczRachunku
         numerPaszportu = numerPaszportu_;
 
     }
-    // pesel_ = pesel ?? throw new Exception("Pesel and passport number cannot be null");
-    // numerPaszportu_ = numerPaszportu ?? throw new Exception("Pesel and passport number cannot be null");
 
     public override string ToString()
     {
@@ -209,16 +208,22 @@ class Program
     static public void Main(string[] args)
     {
         OsobaFizyczna osoba1 = new OsobaFizyczna("Ja", "Ty", "Oni", "11111111111", "AX123543523");
-        OsobaFizyczna osoba2 = new OsobaFizyczna("A", "B", "C", "22222222222", "AX1233432432");
-        OsobaFizyczna osoba3 = new OsobaFizyczna("D", "E", "F", "33333333333", "AX123543523");
-        OsobaFizyczna osoba4 = new OsobaFizyczna("G", "H", "I", "44444444444", "AX123543523");
+        OsobaFizyczna osoba2 = new OsobaFizyczna("A", "B", "C", "22222222222", "AW1233432432");
+        OsobaFizyczna osoba3 = new OsobaFizyczna("D", "E", "F", "33333333333", "AI123543523");
+        OsobaFizyczna osoba4 = new OsobaFizyczna("G", "H", "I", "44444444444", "AL123543523");
 
-        RachunekBankowy r1 = new RachunekBankowy("1234567890", 1000, true, new List<PosiadaczRachunku> { osoba1, osoba2 });
-        RachunekBankowy r2 = new RachunekBankowy("0987654321", 2000, true, new List<PosiadaczRachunku> { osoba3, osoba4 });
+        RachunekBankowy r1 = new RachunekBankowy("1234567890", 1000, false, new List<PosiadaczRachunku> { osoba1, osoba2 });
+        RachunekBankowy r2 = new RachunekBankowy("0987654321", 100.70m, false, new List<PosiadaczRachunku> { osoba3, osoba4 });
         RachunekBankowy r3 = new RachunekBankowy("1234567890", 3000, true, new List<PosiadaczRachunku> { osoba1, osoba2, osoba3, osoba4 });
+        RachunekBankowy.DokonajTransakcji(r2, r1, 0.2m, "Przelew");
+        RachunekBankowy.DokonajTransakcji(r1, r2, 5000m, "Przelew");
 
+        System.Console.WriteLine(r1);
+        System.Console.WriteLine(r2);
+        System.Console.WriteLine(r3);
         
+        r1 -= osoba1;
+        r1 -= osoba1;
 
-        
     }
 }
