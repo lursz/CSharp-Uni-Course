97cc9fce6bbba10bf7a736eded8f5384af8d9b91
lursz <lukoszr@gmail.com>
2023-03-24 11:30:38 +0100

    lab04 fixes

--- lab4/Program.cs
+++ lab4/Program.cs
@@ -30,20 +30,20 @@ class Program
         List<OrderDetail> ordersDetails = new Reader<OrderDetail>().readList("data/orders_details.csv", OrderDetail.MapCsvToModel);
         List<Order> orders = new Reader<Order>().readList("data/orders.csv", Order.MapCsvToModel);
 
-        System.Console.WriteLine("\nSurname of all employees");
+        System.Console.WriteLine("\nLast name of all employees");
         foreach (Employee employee in employees)
         {
             Console.WriteLine(employee.lastname);
         }
 
         
-        System.Console.WriteLine("\nSurname + Region + Territory of all employees");
+        System.Console.WriteLine("\nLast name + Region + Territory of all employees");
         var query2 = from e in employees
                       join et in employeesTerritories on e.employeeid equals et.employeeid
                       join t in territories on et.territoryid equals t.territoryid
                       join r in regions on t.regionid equals r.regionid
                       select new { S = e.lastname, R = r.regiondescription, T = t.territorydescription };
-        foreach (var p in query2.ToList())
+        foreach (var p in query2)
             Console.WriteLine(p.S + " " + p.R + " " + p.T);
             
 
@@ -54,7 +54,7 @@ class Program
                       join r in regions on t.regionid equals r.regionid
                       orderby r.regiondescription
                       select new { R = r.regiondescription, S = e.lastname };
-        foreach (var p in query3.ToList())
+        foreach (var p in query3)
             Console.WriteLine(p.R + " " + p.S);
 
         System.Console.WriteLine("\nRegion + Count of employees working in those regions");
@@ -62,17 +62,17 @@ class Program
                       join et in employeesTerritories on e.employeeid equals et.employeeid
                       join t in territories on et.territoryid equals t.territoryid
                       join r in regions on t.regionid equals r.regionid
-                      group e by r.regiondescription into g
-                      select new { R = g.Key, C = g.Count() };
-        foreach (var p in query4.ToList())
+                      group e by r.regionid into g
+                      select new { R = g.Key, C = g.Distinct().Count() };
+        foreach (var p in query4)
             Console.WriteLine(p.R + " " + p.C);
         
         System.Console.WriteLine("\nCount of orders + average value of order + max value of order for every employee");
         var query5 = from o in orders
                       join od in ordersDetails on o.orderid equals od.orderid
                       group od by o.employeeid into g
-                      select new { E = g.Key, C = g.Count(), A = g.Average(x => x.unitprice * x.quantity), M = g.Max(x => x.unitprice * x.quantity) };
-        foreach (var p in query5.ToList())
+                      select new { E = g.Key, C = g.Count(), A = g.Average(x => x.unitprice * x.quantity - x.discount), M = g.Max(x => x.unitprice * x.quantity - x.discount) };
+        foreach (var p in query5)
             Console.WriteLine(p.E + " " + p.C + " " + p.A + " " + p.M);
 
 
