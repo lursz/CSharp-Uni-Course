e84721f5ecacc0665da2e9c59fbf50ab4c453b31
lursz <lukoszr@gmail.com>
2023-03-31 09:30:57 +0200

    lab05

---
+++ lab5/ProducerConsumer.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    public class ProducerConsumer
+    {
+        public int n { get; set; }
+        public int m { get; set; }
+        
+        public List<Thread
+        public ProducerConsumer(int n, int m)
+        {
+            this.n = n;
+            this.m = m;
+        }
+
+
+
+
+    }
+}
---
+++ lab5/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.InteropServices;
+using System;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
