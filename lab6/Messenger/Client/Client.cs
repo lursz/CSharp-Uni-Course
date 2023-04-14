757c35a28a31aa222f32c8e8e3ff324108c24a1d
lursz <lukoszr@gmail.com>
2023-04-14 09:14:28 +0200

    added lab06

---
+++ lab6/Messenger/Client/Client.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+public class Client
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("Client started");
+        //Server host
+        IPHostEntry host = Dns.GetHostEntry("localhost");
+        //Choosing first address from list
+        IPAddress ipAddress = host.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+        Socket socket = new(
+            localEndPoint.AddressFamily,
+            SocketType.Stream,
+            ProtocolType.Tcp);
+        //Connecting
+        socket.Connect(localEndPoint);
+
+        //Sending message to server in UTF8
+        string wiadomosc = "Wiadomość od klienta";
+        byte[] wiadomoscBajty = Encoding.UTF8.GetBytes(wiadomosc);
+        socket.Send(wiadomoscBajty, SocketFlags.None);
+
+        //Buffer for message, max 1024 bytes
+        var bufor = new byte[1_024];
+        //Receiving message from server
+        int liczbaBajtów = socket.Receive(bufor, SocketFlags.None);
+        String odpowiedzSerwera = Encoding.UTF8.GetString(bufor, 0, liczbaBajtów);
+
+        Console.WriteLine(odpowiedzSerwera);
+        try
+        {
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+        catch { }
+
+    }
+
+}
---
+++ lab6/Messenger/Server/Server.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+public class Server
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("Server started");
+        IPHostEntry host = Dns.GetHostEntry("localhost");
+        //Choosing first address from list
+        IPAddress ipAddress = host.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+        //Socket listening on TCP/IP
+        Socket socketSerwera = new(
+            localEndPoint.AddressFamily,
+            SocketType.Stream,
+            ProtocolType.Tcp);
+        //Port reservation
+        socketSerwera.Bind(localEndPoint);
+        //Begin listening
+        socketSerwera.Listen(100);
+        //Awaiting connections
+        Socket socketKlienta = socketSerwera.Accept();
+        // Buffer for message, max 1024 bytes
+        byte[] bufor = new byte[1_024];
+
+        //Blocking call, waiting for message
+        int received = socketKlienta.Receive(bufor, SocketFlags.None);
+        String wiadomoscKlienta = Encoding.UTF8.GetString(bufor, 0, received);
+        Console.WriteLine(wiadomoscKlienta);
+        string odpowiedz = "odpowiedź serwera";
+        var echoBytes = Encoding.UTF8.GetBytes(odpowiedz);
+        socketKlienta.Send(echoBytes, 0);
+        try
+        {
+            socketSerwera.Shutdown(SocketShutdown.Both);
+            socketSerwera.Close();
+        }
+        catch { }
+    }
+
+}
+
+
